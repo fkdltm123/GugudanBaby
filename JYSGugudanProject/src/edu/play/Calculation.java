3c26c912b68bbb0bdc288c7b3d48f566280a818a
KOREAIT <KOREAIT@DESKTOP-7URGO06>
2021-10-25 17:10:04 +0900

    구구단 프로젝트를 만들었습니다 이어서 만들어주세요 to PHJ

---
+++ JYSGugudanProject/src/edu/play/Calculation.java
@@ -0,0 +1,5 @@
+package edu.play;
+
+public class Calculation {
+
+}
---
+++ JYSGugudanProject/src/edu/play/GugudanTest.java
@@ -0,0 +1,10 @@
+package edu.play;
+
+public class GugudanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
