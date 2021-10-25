429767fb7e6fa254cf4ed7010cd4487808ee52b9
KOREAIT <KOREAIT@DESKTOP-7URGO07>
2021-10-25 17:21:27 +0900

    2번째 구구단

--- JYSGugudanProject/src/edu/play/Calculation.java
+++ JYSGugudanProject/src/edu/play/Calculation.java
@@ -1,11 +1,37 @@
 package edu.play;
 
 public class Calculation {
-	public void Gugudan1(){
-		for(int i=2;i<=9;i++) {
-			for(int j=1;j<=9;j++) {
-				System.out.println(i*j);
+
+	public void Gugudan1() {
+		
+		for (int i = 2; i <= 9; i++) {
+			
+			for (int j = 1; j <= 9; j++) {
+				
+				System.out.println(i * j);
 			}
+			
+		}
+		
+	}//void
+	
+	
+	
+	
+	public void verticalGugu() {
+		
+		for (int i = 1; i <= 9; i++) {
+			
+			for (int j = 2; j <= 9; j++) {
+				
+				System.out.print(i * j);
+				
+				System.out.print(" ");
+			}
+			
+			System.out.println();
 		}
 	}
+	
+	
 }
