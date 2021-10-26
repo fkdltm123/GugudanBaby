4ef5959779cb0f6065d203f5a6d4d121b22b6394
KOREAIT <KOREAIT@DESKTOP-7URGO02>
2021-10-26 14:21:17 +0900

    gugudan1() 수정 from kjh

--- JYSGugudanProject/src/edu/play/Calculation.java
+++ JYSGugudanProject/src/edu/play/Calculation.java
@@ -8,8 +8,9 @@ public class Calculation {
 			
 			for (int j = 1; j <= 9; j++) {
 				
-				System.out.println(i * j);
+				System.out.print(i * j + " ");
 			}
+			System.out.println();
 			
 		}
 		
